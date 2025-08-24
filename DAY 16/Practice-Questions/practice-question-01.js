cccd395884aa87d3459160aa53763f24d4ca7d01
Shubham Chaudhary <90134622+ritesh23s@users.noreply.github.com>
2025-08-24 15:30:26 +0530

    Delete DAY 16 directory

--- DAY 16/Practice-Questions/practice-question-01.js
+++
@@ -1,5 +0,0 @@
-// Question = For the give string let msg = "help!"; Trim it and convert it to uppercase
-// Answer = 
-let msg = "hello!";
-let newMsg = msg.trim().toUpperCase();
-console.log(newMsg);
